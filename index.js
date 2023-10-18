7bfa1cd94fa33ced5294446238633c325f605a12
Harry Stanton <harry@harrystanton.com>
2023-10-17 22:52:43 -0400

    feat: dummy dashboard w/ completed new forms

--- index.js
+++ index.js
@@ -10,14 +10,11 @@ app.get("/dashboard/issuers", (req, res) => {
         title: "Global Issuers",
         subtitle: "0 issuers",
         new_url: "/dashboard/issuers/new",
-        table: {
-            headers: [ "Name", "ID", "BINs", "Thumbnail" ]
-        },
         js: [
             "/static/js/dashboard/issuers.js"
         ]
     }
-    res.render("table", opts)
+    res.render("table/global_issuers", opts)
 })
 
 app.get("/dashboard/issuers/new", (req, res) => {
@@ -31,6 +28,56 @@ app.get("/dashboard/issuers/new", (req, res) => {
         ]
     }
     res.render("new/global_issuer", opts)
+})
+
+app.get("/dashboard/paymentmethods", (req, res) => {
+    const opts = {
+        id: "payment_methods",
+        title: "Global Payment Methods",
+        subtitle: "0 global payment methods",
+        new_url: "/dashboard/paymentmethods/new",
+        js: [
+            "/static/js/dashboard/issuers.js"
+        ]
+    }
+    res.render("table/global_payment_methods", opts)
+})
+
+app.get("/dashboard/paymentmethods/new", (req, res) => {
+    const opts = {
+        id: "payment_methods",
+        title: "New Global Payment Method",
+        subtitle: "Create a new payment method",
+        back_url: "/dashboard/paymentmethods",
+        js: [
+            "/static/js/dashboard/issuers.js"
+        ]
+    }
+    res.render("new/global_payment_method", opts)
+})
+
+app.get("/dashboard/promotions", (req, res) => {
+    const opts = {
+        id: "cashback_promotions",
+        new_url: "/dashboard/promotions/new",
+        js: [
+            "/static/js/dashboard/issuers.js"
+        ]
+    }
+    res.render("table/cashback_promotions", opts)
+})
+
+app.get("/dashboard/promotions/new/", (req, res) => {
+    const opts = {
+        id: "cashback_promotions",
+        title: "New Cashback Promotion",
+        subtitle: "Create a new cashback promotion",
+        back_url: "/dashboard/promotions",
+        js: [
+            "/static/js/dashboard/issuers.js"
+        ]
+    }
+    res.render("new/cashback_promotion", opts)
 })
 
 const port = process.env.PORT || 3000
